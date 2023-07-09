a1c23dc67113e7c42609107239258398759c7d2a
Bijoy Kumar Biswas <bijoybiswas27@gmail.com>
2023-07-09 17:02:38 +0530

    Implemented all the CRUD operations for Bootcamp model

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,27 +1,118 @@
+const BootcampModel = require("./../models/Bootcamp");
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-const getAllBootcamps = (req, res, next) => {};
+const getAllBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await BootcampModel.find();
+    res.status(200).json({
+      success: true,
+      count: bootcamps.length,
+      data: bootcamps,
+    });
+  } catch (err) {
+    console.log("Error: " + err);
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-const getBootcampById = (req, res, next) => {};
+const getBootcampById = async (req, res, next) => {
+  try {
+    const bootcamp = await BootcampModel.findOne({
+      _id: req.params.id,
+    });
+    if (!bootcamp) {
+      return res.status(404).json({
+        success: false,
+        message: `Bootcamp with id: ${req.params.id} is not found`,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    console.log("Error: " + err);
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-const createBootcamp = (req, res, next) => {};
+const createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await BootcampModel.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    console.log("Error: " + err);
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-const updateBootcampById = (req, res, next) => {};
+const updateBootcampById = async (req, res, next) => {
+  try {
+    const bootcamp = await BootcampModel.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (!bootcamp) {
+      return res.status(404).json({
+        success: false,
+        message: `Bootcamp with id: ${req.params.id} is not found`,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    console.log("Error: " + err);
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-const deleteBootcampById = (req, res, next) => {};
+const deleteBootcampById = async (req, res, next) => {
+  try {
+    const bootcamp = await BootcampModel.findByIdAndDelete(req.params.id);
+    if (!bootcamp) {
+      return res.status(404).json({
+        success: false,
+        message: `Bootcamp with id: ${req.params.id} is not found`,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (err) {
+    console.log("Error: " + err);
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
 
 module.exports = {
   getAllBootcamps,
---
+++ models/Bootcamp.js
@@ -0,0 +1,78 @@
+const mongoose = require("mongoose");
+
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+    unique: true,
+    trim: true,
+    maxlength: [50, "Name cannot be more than 50 characters"],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, "Please add a description"],
+    maxlength: [500, "Description cannot be more than 500 characters"],
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      "Please use a valid URL with HTTP or HTTPS",
+    ], // if it doesn't match the regex, we will get the error message
+  },
+  phone: {
+    type: String,
+    maxlength: [20, "Phone number cannot be longer than 20 characters"],
+  },
+  address: {
+    type: String,
+    required: [true, "Please add an address"],
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      // required: true,
+    },
+    coordinates: {
+      type: [Number], // Array of numbers
+      // required: true,
+      index: "2dsphere",
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  careers: {
+    type: [String],
+    required: true,
+    enum: [
+      "Web Development",
+      "Mobile Development",
+      "UI/UX",
+      "Data Science",
+      "Business",
+      "Others",
+    ],
+  },
+  averageRating: {
+    type: Number,
+    min: [1, "Rating must be at least 1"],
+    max: [10, "Rating must cannot be more than 10"],
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: "no-photo.jpg",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Bootcamp", BootcampSchema);
